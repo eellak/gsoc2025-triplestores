b34e20b251198d6a9d297d99eeafc3c087407923
mairacs <mmpapadopoulouu@gmail.com>
2025-08-27 14:14:06 +0300

    Micro Fixes at Oxigraph Demo
    Signed-off-by: mairacs <mmpapadopoulouu@gmail.com>

--- triplestore/demo/oxigraph.py
+++ triplestore/demo/oxigraph.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2025
+# Copyright (C) 2025 Maira Papadopoulou
 # SPDX-License-Identifier: Apache-2.0
 
 from triplestore import TriplestoreFactory
@@ -7,18 +7,24 @@ config = {
     "graph": "http://example.org/test"
 }
 
-store = TriplestoreFactory("oxigraph", config=config)
-print(" Loading data…")
-store.load("triplestore/data.ttl")
 
-num_of_triples_query = "SELECT (COUNT(*) AS ?count) WHERE { GRAPH <http://example.org/test> { ?s ?p ?o } }"
-result = store.execute(num_of_triples_query)
-num_of_triples = result[0]["count"].value
-print(f" Loaded. Triple count in <http://example.org/test>: {int(num_of_triples):,}")
+def main() -> None:
+    store = TriplestoreFactory("oxigraph", config=config)
 
-print("\n Clearing graph…")
-store.clear()
-num_of_triples_query = "SELECT (COUNT(*) AS ?count) WHERE { GRAPH <http://example.org/test> { ?s ?p ?o } }"
-result = store.execute(num_of_triples_query)
-num_of_triples = result[0]["count"].value
-print(f" Cleared. Triple count in <http://example.org/test>: {int(num_of_triples):,}")
+    print(" Loading data…")
+    store.load("triplestore/data.ttl")
+
+    num_of_triples_query = "SELECT (COUNT(*) AS ?count) WHERE { GRAPH <http://example.org/test> { ?s ?p ?o } }"
+    result = store.execute(num_of_triples_query)
+    num_of_triples = result[0]["count"].value
+    print(f" Loaded. Triple count in <http://example.org/test>: {int(num_of_triples):,}")
+
+    print("\n Clearing graph…")
+    store.clear()
+    result = store.execute(num_of_triples_query)
+    num_of_triples = result[0]["count"].value
+    print(f" Cleared. Triple count in <http://example.org/test>: {int(num_of_triples):,}")
+
+
+if __name__ == "__main__":
+    main()
